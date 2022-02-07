2a1d28cb84fc8607623130eb897d9ef3e415a8a8
Deepak Agarwal <deepak710agarwal@gmail.com>
2022-02-07 09:18:10 +0530

    Add files via upload

---
+++ scripts/index.js
@@ -0,0 +1,24 @@
+// function to change image to dog image
+function changeToDogImage() {
+    document.getElementById('myImage')
+    .src="https://s3-ap-southeast-1.amazonaws.com/he-public-data/Dog%20image69059f5.jpg";
+    document.getElementById('message')
+    .innerHTML="Animal is Dog";
+}
+
+// function to change image to cat image
+function changeToCatImage() {
+    document.getElementById('myImage')
+    .src="https://s3-ap-southeast-1.amazonaws.com/he-public-data/At%20image7532683.jpg";
+    document.getElementById('message')
+    .innerHTML="Animal is cat";
+}
+
+// function to change image to parrot image
+function changeToParrotImage() {
+    document.getElementById('myImage')
+    .src="https://s3-ap-southeast-1.amazonaws.com/he-public-data/Parrot%20image7f2c45e.jpg";
+    document.getElementById('message')
+    .innerHTML="Animal is Parrot";
+}
+
