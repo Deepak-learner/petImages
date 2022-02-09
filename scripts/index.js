3744b8ad32d3e90796c7508f07207835e6a4e327
Deepak Agarwal <deepak710agarwal@gmail.com>
2022-02-09 07:14:10 +0530

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,24 +1,18 @@
-// function to change image to dog image
-function changeToDogImage() {
-    document.getElementById('myImage')
-    .src="https://s3-ap-southeast-1.amazonaws.com/he-public-data/Dog%20image69059f5.jpg";
-    document.getElementById('message')
-    .innerHTML="Animal is Dog";
-}
+    function changePetImage(petImageNumber) {
+        document.getElementById('petImage').src="assets/pet"+petImageNumber+".jpeg";
+        switch (petImageNumber) {
+        case 1: 
+            document.getElementById('message').innerHTML="Animal is Cat!";
+            break;
 
-// function to change image to cat image
-function changeToCatImage() {
-    document.getElementById('myImage')
-    .src="https://s3-ap-southeast-1.amazonaws.com/he-public-data/At%20image7532683.jpg";
-    document.getElementById('message')
-    .innerHTML="Animal is cat";
-}
+        case 2: 
+            document.getElementById('message').innerHTML="Animal is Dog!";
+            break;
 
-// function to change image to parrot image
-function changeToParrotImage() {
-    document.getElementById('myImage')
-    .src="https://s3-ap-southeast-1.amazonaws.com/he-public-data/Parrot%20image7f2c45e.jpg";
-    document.getElementById('message')
-    .innerHTML="Animal is Parrot";
-}
+        default:
+            document.getElementById('message').innerHTML="Animal is Parrot!";
+            break;    
+        }
+    }
+
 
