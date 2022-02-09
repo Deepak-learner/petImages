fa75e167eb2d7cdb6ec788d2c1fabeea8abd3ddf
Deepak Agarwal <deepak710agarwal@gmail.com>
2022-02-09 08:29:09 +0530

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,6 @@
-    function changePetImage(petImageNumber) {
-        document.getElementById('petImage').src="assets/pet"+petImageNumber+".jpeg";
-        switch (petImageNumber) {
-        case 1: 
-            document.getElementById('message').innerHTML="Animal is Cat!";
-            break;
-
-        case 2: 
-            document.getElementById('message').innerHTML="Animal is Dog!";
-            break;
-
-        default:
-            document.getElementById('message').innerHTML="Animal is Parrot!";
-            break;    
-        }
-    }
+function changePetImage(petName) {
+    document.getElementById('petImage').src="assets/"+petName+".jpeg";
+    document.getElementById('message').innerHTML="Animal is " + petName;
+}
 
 
