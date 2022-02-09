80f7c391476220e7fd23a7a3d1b6546e8073a519
Deepak Agarwal <deepak710agarwal@gmail.com>
2022-02-09 10:45:10 +0530

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,6 @@
 function changePetImage(petName) {
     document.getElementById('petImage').src="assets/"+petName+".jpeg";
-    document.getElementById('message').innerHTML="Animal is " + petName;
+    document.getElementById('message').innerHTML="Animal is " + petName + "!";
 }
 
 
